234d0e76f708a021e2487b2f15ee532d5e874384
ysubli <163995939+ysubli@users.noreply.github.com>
2025-10-17 16:03:36 +0200

    Delete gitcidevB-main/AddTask.js

--- gitcidevB-main/AddTask.js
+++
@@ -1,7 +0,0 @@
-const { tasks } = require('./tasks');
-function addTask(name) {
-  if (!name || !name.trim()) return false;
-  tasks.push({ name: name.trim(), done: false });
-  return true;
-}
-module.exports = addTask;
