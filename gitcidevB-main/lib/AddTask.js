1d3340fc6a68466637328fc8091704f3564fcabb
sidy Sylla <Sylla.sidy77680@gmail.com>
2025-10-17 16:42:47 +0200

    Create AddTask.js
    Envoi de Addtask, cette fonction ajoute des taches dans la liste et va couper les espaces

---
+++ gitcidevB-main/lib/AddTask.js
@@ -0,0 +1,9 @@
+const { tasks } = require('../tasks');
+
+function addTask(name) {
+  if (!name || !name.trim()) return false;
+  tasks.push({ name: name.trim(), done: false });
+  return true;
+}
+
+module.exports = addTask;
